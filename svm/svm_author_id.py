113ed987e14cbec7471740f1992caa966d865fa8
Jeremiah Perry <jeremiah.perry@gmail.com>
2015-12-23 21:40:38 -0600

    Work for SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+## cut down the data size
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+
+## linear
+#clf = SVC(kernel='linear')
+## radial
+#for C in [10, 100, 1000, 10000]:
+clf = SVC(kernel='rbf', C=10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+print "accuracy:", acc, "C:", C
+
+
 #########################################################
 
 
